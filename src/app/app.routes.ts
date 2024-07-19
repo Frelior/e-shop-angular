9018abb82b14316e68d0016a18b2814d359e51d1
Michael Rudenko <annoyedmichael@gmail.com>
2024-07-19 15:25:22 +0300

    making header component

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HeaderComponent,
+  },
+];
